0933ba40ef5fc69359f5f7829a0c8a6934e3f7a2
rma945 <6511900+rma945@users.noreply.github.com>
2025-10-13 20:50:27 +0700

    feat: add option to set database path

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -20,16 +20,21 @@ func main() {
 		TGBotToken   string `default:"" arg:"--token,env:TG_TOKEN" help:"telegram bot token"`
 		TGHost       string `default:"http://127.0.0.1:8081" arg:"--tgapi,env:TG_HOST" help:"telegram api host"`
 		TSHost       string `default:"http://127.0.0.1:8090" arg:"--ts,env:TS_HOST" help:"TorrServer host"`
+		DBPath       string `default:"" arg:"--db-path,env:DB_PATH" help:"Database path (Default to cwd)"`
 		DBHost       string `default:"http://62.112.8.193:9117" arg:"--db,env:DB_HOST" help:"External sync database host"`
 		DBSync       int    `default:"20" arg:"--db-sync,env:DB_SYNC" help:"External database sync delay"`
 		DBSyncRetry  int    `default:"10" arg:"--db-sync-retry,env:DB_SYNC_RETRY" help:"External database sync retry"`
 	}
 	arg.MustParse(&args)
 
-	pwd := filepath.Dir(os.Args[0])
-	pwd, _ = filepath.Abs(pwd)
-	log.Println("PWD:", pwd)
-	global.PWD = pwd
+	if args.DBPath != "" {
+		global.PWD = args.DBPath
+	} else {
+		pwd := filepath.Dir(os.Args[0])
+		pwd, _ = filepath.Abs(pwd)
+		global.PWD = pwd
+	}
+	log.Println("Database bath set to:", global.PWD)
 
 	global.TMDBProxy = args.TMDBProxy
 	global.TSHost = args.TSHost
